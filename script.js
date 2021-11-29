df63d4fd575ba57f6770951de12ca432e3ba8cd7
Dylan Lokatys <dlokatys@hotmail.com>
2021-11-29 18:45:19 -0500

    simplified layout, added interactive pie chart

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let weeklyIncome = 0;
 let totalExpenses = 0;
 let remainingBalance = 0;
 
-let employmentIncome = document.getElementById('employmentIncomeInput');
-let passiveIncome = document.getElementById('passiveIncomeInput');
+let weeklyIncome = document.getElementById('weeklyIncome');
+let weeklyExpense = document.getElementById('weeklyExpense');
 
 
 

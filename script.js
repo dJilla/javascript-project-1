e20d29080c479d45d65e8dff97025fd6693b1ce3
Dylan Lokatys <dlokatys@hotmail.com>
2021-11-29 20:12:44 -0500

    cleaned up some code in js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 (function() {
 
-let expenseList = [];
+//selectors
+let weeklyIncomeDisplay = document.querySelector('#weeklyIncome');
+let weeklyIncomeInput = document.querySelector('#weeklyIncomeInput');
+let updateIncomeButton = document.querySelector('#updateIncomeButton');
+let expenseDropdown = document.querySelector('#expenseDropdown');
+let weeklyExpenseInput = document.querySelector('#weeklyExpenseInput');
+
+//variables
 let weeklyIncome = 0;
-let totalExpenses = 0;
-let remainingBalance = 0;
+let billsExpenses = [];
+let billsExpenseTotal = 0;
+let foodExpenses = [];
+let foodExpenseTotal = 0;
+let clothingExpenses = [];
+let clothingExpenseTotal = 0;
+let entertainmentExpenses = [];
+let entertainmentExpenseTotal = 0;
+let miscExpenses = [];
+let miscExpenseTotal = 0;
+let balance = 0;
 
-let weeklyIncome = document.getElementById('weeklyIncome');
-let weeklyExpense = document.getElementById('weeklyExpense');
-
-
+updateIncomeButton.addEventListener('click', event => {
+    console.log('this is working')
+    weeklyIncome = weeklyIncomeInput.value;
+    weeklyIncomeDisplay.innerText = `Weekly Income: $${weeklyIncome};`
+    event.preventDefault();
+})
 
 })();

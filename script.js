a306585eaf462e9223e54b8ca12c34346f0d6110
Dylan Lokatys <dlokatys@hotmail.com>
2021-11-29 22:03:48 -0500

    added functionality to both buttons and inputs, able to determine class by select dropdown

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 (function() {
 
 //selectors
-let weeklyIncomeDisplay = document.querySelector('#weeklyIncome');
+let weeklyIncomeDisplay = document.querySelector('#weeklyIncomeDisplay');
 let weeklyIncomeInput = document.querySelector('#weeklyIncomeInput');
 let updateIncomeButton = document.querySelector('#updateIncomeButton');
 let expenseDropdown = document.querySelector('#expenseDropdown');
 let weeklyExpenseInput = document.querySelector('#weeklyExpenseInput');
+let addExpenseButton = document.querySelector('#addExpenseButton');
 
 //variables
 let weeklyIncome = 0;
@@ -21,11 +22,55 @@ let miscExpenses = [];
 let miscExpenseTotal = 0;
 let balance = 0;
 
+//event listener on income button
 updateIncomeButton.addEventListener('click', event => {
-    console.log('this is working')
-    weeklyIncome = weeklyIncomeInput.value;
-    weeklyIncomeDisplay.innerText = `Weekly Income: $${weeklyIncome};`
     event.preventDefault();
-})
+
+    //declaring weeklyIncome to be equal to the value of the input
+    weeklyIncome = weeklyIncomeInput.value;
+
+    //displaying 'Weekly Income:' and then value of the income
+    weeklyIncomeDisplay.innerText = `Weekly Income: $${weeklyIncome}`;
+});
+
+//event listener on expense button
+addExpenseButton.addEventListener('click', event => {
+    event.preventDefault();
+
+    //add variable to store input value
+    let expense = {
+        name: weeklyExpenseName.value,
+        amount: weeklyExpenseInput.value
+    };
+
+    //create new element for the expense to display
+    let newExpense = document.createElement('p');
+    newExpense.innerText = `${expense.name}: $${expense.amount}`;
+    expenseSection.append(newExpense);
+
+    //add variable to determine selection
+    let selection = parseInt(expenseDropdown.value);
+
+    //add a class depending on selection
+    if (selection === 4) {
+
+        newExpense.classList.add('billsClass');
+        console.log('this is setting to bills class')
+    } else if (selection === 3) {
+        
+        newExpense.classList.add('foodClass');
+    } else if (selection === 2) {
+
+        newExpense.classList.add('clothingClass');
+    } else if (selection === 1) {
+
+        newExpense.classList.add('entertainmentClass');
+    } else if (selection === 0) {
+
+        newExpense.classList.add('miscClass');
+    };
+    
+    
+});
 
 })();
